9a51ed2d4f4a50a36d06f076fcb1608ef6e320f4
zk <982847942@qq.com>
2021-03-16 10:03:20 +0800

    坦克大战

---
+++ src/com/zk/tank/Group.java
@@ -0,0 +1,5 @@
+package com.zk.tank;
+
+public enum Group {
+	GOOD, BAD
+}
---
+++ src/com/zk/tank/net/MsgType.java
@@ -0,0 +1,6 @@
+package com.zk.tank.net;
+//消息类型
+public enum MsgType {
+	TankJoin, TankDirChanged, TankStop, TankStartMoving, BulletNew, TankDie
+}
+
